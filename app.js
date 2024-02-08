3513d377bdb6e49ac12b13665610f0a5493701da
Mohit Raj Parit <mohitrajparit@pop-os.localdomain>
2024-02-08 22:02:42 +0530

    code upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const app=express();
+const tasks=require('./routes/tasks')
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
+//middleware
+app.use(express.static('./public'));
+app.use(express.json());
+//routes
+app.use('/api/v1/tasks',tasks);
+const port=process.env.port || 5000;
+const start=async()=>{
+    try{
+        await connectDB(process.env.MONGO_URI);
+        app.listen(5000,(req,res)=>{
+            console.log("`App is listening on port 5000")
+        })
+    }   
+    catch(error){
+        console.log(error);
+    }
+}
+start();
